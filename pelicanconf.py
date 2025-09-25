5ea763d989da9c93897bb1b1e278180a4acdb5e9
jaerrib <jaerrib@tutanota.com>
2025-09-25 12:59:19 -0400

    Change output directory, add social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,11 +25,15 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Mastodon", "https://mastodon.social/@johnbeers"),
+    ("Codeberg", "https://codeberg.org/jaerrib"),
+    ("GitHub", "https://github.com/jaerrib/"),
+    ("LinkedIn", "https://www.linkedin.com/in/john-beers9/")
 )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+OUTPUT = "."
